b8d80c0fb8276e7d58db680ad8b21ff770f8171b
noe hernandez <noe_5991@hotmail.com>
2019-01-02 17:43:26 -0600

    creacion de servicios y conexion a BD
    creacion de servicios y conexion a BD

---
+++ SGCVServicios/src/main/java/com/sgcv/dao/Trabajador.java
@@ -0,0 +1,248 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sgcv.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Ernesto
+ */
+@Entity
+@Table(name = "trabajador")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Trabajador.findAll", query = "SELECT t FROM Trabajador t")
+    , @NamedQuery(name = "Trabajador.findByIdTrabajador", query = "SELECT t FROM Trabajador t WHERE t.idTrabajador = :idTrabajador")
+    , @NamedQuery(name = "Trabajador.findByUrlFoto", query = "SELECT t FROM Trabajador t WHERE t.urlFoto = :urlFoto")
+    , @NamedQuery(name = "Trabajador.findByNumeroLicencia", query = "SELECT t FROM Trabajador t WHERE t.numeroLicencia = :numeroLicencia")
+    , @NamedQuery(name = "Trabajador.findByVigenciaLicencia", query = "SELECT t FROM Trabajador t WHERE t.vigenciaLicencia = :vigenciaLicencia")
+    , @NamedQuery(name = "Trabajador.findByNssTrabajador", query = "SELECT t FROM Trabajador t WHERE t.nssTrabajador = :nssTrabajador")
+    , @NamedQuery(name = "Trabajador.findByFechaIngreso", query = "SELECT t FROM Trabajador t WHERE t.fechaIngreso = :fechaIngreso")
+    , @NamedQuery(name = "Trabajador.findBySueldo", query = "SELECT t FROM Trabajador t WHERE t.sueldo = :sueldo")
+    , @NamedQuery(name = "Trabajador.findBySueldoIntegrado", query = "SELECT t FROM Trabajador t WHERE t.sueldoIntegrado = :sueldoIntegrado")
+    , @NamedQuery(name = "Trabajador.findByPuesto", query = "SELECT t FROM Trabajador t WHERE t.puesto = :puesto")
+    , @NamedQuery(name = "Trabajador.findByRiesgoTrabajo", query = "SELECT t FROM Trabajador t WHERE t.riesgoTrabajo = :riesgoTrabajo")
+    , @NamedQuery(name = "Trabajador.findByTipoContrato", query = "SELECT t FROM Trabajador t WHERE t.tipoContrato = :tipoContrato")
+    , @NamedQuery(name = "Trabajador.findByLocalidadLaboral", query = "SELECT t FROM Trabajador t WHERE t.localidadLaboral = :localidadLaboral")})
+public class Trabajador implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_trabajador")
+    private Integer idTrabajador;
+    @Size(max = 1)
+    @Column(name = "url_foto")
+    private String urlFoto;
+    @Size(max = 1)
+    @Column(name = "numero_licencia")
+    private String numeroLicencia;
+    @Size(max = 1)
+    @Column(name = "vigencia_licencia")
+    private String vigenciaLicencia;
+    @Size(max = 1)
+    @Column(name = "nss_trabajador")
+    private String nssTrabajador;
+    @Size(max = 1)
+    @Column(name = "fecha_ingreso")
+    private String fechaIngreso;
+    @Column(name = "sueldo")
+    private Integer sueldo;
+    @Column(name = "sueldo_integrado")
+    private Integer sueldoIntegrado;
+    @Size(max = 1)
+    @Column(name = "puesto")
+    private String puesto;
+    @Size(max = 1)
+    @Column(name = "riesgo_trabajo")
+    private String riesgoTrabajo;
+    @Size(max = 1)
+    @Column(name = "tipo_contrato")
+    private String tipoContrato;
+    @Size(max = 1)
+    @Column(name = "localidad_laboral")
+    private String localidadLaboral;
+    @JoinColumn(name = "id_direccion", referencedColumnName = "id_direccion")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Direccion idDireccion;
+    @JoinColumn(name = "id_persona", referencedColumnName = "id_persona")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Persona idPersona;
+    @OneToMany(mappedBy = "idTrabajador", fetch = FetchType.LAZY)
+    private List<Servicio> servicioList;
+
+    public Trabajador() {
+    }
+
+    public Trabajador(Integer idTrabajador) {
+        this.idTrabajador = idTrabajador;
+    }
+
+    public Integer getIdTrabajador() {
+        return idTrabajador;
+    }
+
+    public void setIdTrabajador(Integer idTrabajador) {
+        this.idTrabajador = idTrabajador;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+
+    public String getNumeroLicencia() {
+        return numeroLicencia;
+    }
+
+    public void setNumeroLicencia(String numeroLicencia) {
+        this.numeroLicencia = numeroLicencia;
+    }
+
+    public String getVigenciaLicencia() {
+        return vigenciaLicencia;
+    }
+
+    public void setVigenciaLicencia(String vigenciaLicencia) {
+        this.vigenciaLicencia = vigenciaLicencia;
+    }
+
+    public String getNssTrabajador() {
+        return nssTrabajador;
+    }
+
+    public void setNssTrabajador(String nssTrabajador) {
+        this.nssTrabajador = nssTrabajador;
+    }
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public Integer getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Integer sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public Integer getSueldoIntegrado() {
+        return sueldoIntegrado;
+    }
+
+    public void setSueldoIntegrado(Integer sueldoIntegrado) {
+        this.sueldoIntegrado = sueldoIntegrado;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getRiesgoTrabajo() {
+        return riesgoTrabajo;
+    }
+
+    public void setRiesgoTrabajo(String riesgoTrabajo) {
+        this.riesgoTrabajo = riesgoTrabajo;
+    }
+
+    public String getTipoContrato() {
+        return tipoContrato;
+    }
+
+    public void setTipoContrato(String tipoContrato) {
+        this.tipoContrato = tipoContrato;
+    }
+
+    public String getLocalidadLaboral() {
+        return localidadLaboral;
+    }
+
+    public void setLocalidadLaboral(String localidadLaboral) {
+        this.localidadLaboral = localidadLaboral;
+    }
+
+    public Direccion getIdDireccion() {
+        return idDireccion;
+    }
+
+    public void setIdDireccion(Direccion idDireccion) {
+        this.idDireccion = idDireccion;
+    }
+
+    public Persona getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Persona idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    @XmlTransient
+    public List<Servicio> getServicioList() {
+        return servicioList;
+    }
+
+    public void setServicioList(List<Servicio> servicioList) {
+        this.servicioList = servicioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idTrabajador != null ? idTrabajador.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Trabajador)) {
+            return false;
+        }
+        Trabajador other = (Trabajador) object;
+        if ((this.idTrabajador == null && other.idTrabajador != null) || (this.idTrabajador != null && !this.idTrabajador.equals(other.idTrabajador))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.sgcv.dao.Trabajador[ idTrabajador=" + idTrabajador + " ]";
+    }
+    
+}
---
+++ SGCVServicios/src/main/java/com/sgcv/dao/Usuario.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sgcv.dao;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Ernesto
+ */
+@Entity
+@Table(name = "usuario")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u")
+    , @NamedQuery(name = "Usuario.findById", query = "SELECT u FROM Usuario u WHERE u.id = :id")
+    , @NamedQuery(name = "Usuario.findByNombre", query = "SELECT u FROM Usuario u WHERE u.nombre = :nombre")
+    , @NamedQuery(name = "Usuario.findByCorreo", query = "SELECT u FROM Usuario u WHERE u.correo = :correo")
+    , @NamedQuery(name = "Usuario.findByClave", query = "SELECT u FROM Usuario u WHERE u.clave = :clave")})
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 1)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 1)
+    @Column(name = "correo")
+    private String correo;
+    @Size(max = 1)
+    @Column(name = "clave")
+    private String clave;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.sgcv.dao.Usuario[ id=" + id + " ]";
+    }
+    
+}
