d96a0c228a619d85fcd05e0cb6e5f349fbd624a5
Ferruz86 <45954269+Ferruz86@users.noreply.github.com>
2019-01-20 22:57:45 -0600

    nueva_actualizacion
    nueva actualizacion

---
+++ SGCVServicios/src/main/java/com/sgcv/bean/GestorUsuariosBean.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sgcv.bean;
+
+import com.sgcv.dao.Dao;
+import com.sgcv.dao.Parametros;
+import com.sgcv.dao.Usuario;
+import com.sgcv.dto.UsuarioDTO;
+import com.sgcv.utils.CONSTANTE;
+import com.sgcv.utils.Encrypt;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Ernesto
+ */
+public class GestorUsuariosBean {
+    
+    public String insertaUsuario(UsuarioDTO usuarioDTO){
+        Usuario usuario= new Usuario();
+        usuario.setClave(usuarioDTO.getClave());
+        usuario.setCorreo(usuarioDTO.getCorreo());
+        usuario.setNombre(usuarioDTO.getNombre());
+        Dao<Usuario> dao= new Dao<Usuario>(Usuario.class);
+        boolean result = dao.inserta(usuario);
+        System.out.println("Total "+result);
+        return ""+result;
+    }
+    
+    public String modificaUsuario(UsuarioDTO usuarioDTO){
+        Usuario usuario= new Usuario();
+        usuario.setClave(usuarioDTO.getClave());
+        usuario.setCorreo(usuarioDTO.getCorreo());
+        usuario.setNombre(usuarioDTO.getNombre());
+        usuario.setId(usuarioDTO.getId());
+        Dao<Usuario> dao= new Dao<Usuario>(Usuario.class);
+        boolean result = dao.actualiza(usuario);
+        System.out.println("Total "+result);
+        return ""+result;
+    }
+    
+//    public String consultaUsuarioById(String idUsuario){
+//        Usuario usuario= new Usuario();
+//        usuario.setClave(usuarioDTO.getClave());
+//        usuario.setCorreo(usuarioDTO.getCorreo());
+////        usuario.setId(Integer.SIZE);
+//        usuario.setNombre(usuarioDTO.getNombre());
+//        Dao<Usuario> dao= new Dao<Usuario>(Usuario.class);
+//        boolean result = dao.inserta(usuario);
+//        System.out.println("Total "+result);
+//        return ""+result;
+//    }
+    
+    public String eliminaUsuario(UsuarioDTO usuarioDTO){
+        Usuario usuario= new Usuario();
+        usuario.setClave(usuarioDTO.getClave());
+        usuario.setCorreo(usuarioDTO.getCorreo());
+//        usuario.setId(Integer.SIZE);
+        usuario.setNombre(usuarioDTO.getNombre());
+        Dao<Usuario> dao= new Dao<Usuario>(Usuario.class);
+        boolean result = dao.inserta(usuario);
+        System.out.println("Total "+result);
+        return ""+result;
+    }
+    
+    public String login(UsuarioDTO usuarioDTO){
+        String user=usuarioDTO.getNombre();
+        String pass=usuarioDTO.getClave();
+        List<Usuario> usuarios=null;
+        try {
+            List<Parametros> parametros= new ArrayList<>();
+            parametros.add(new Parametros(CONSTANTE.CADENA,user,"nombre"));
+//            parametros.add(new Parametros(CONSTANTE.CADENA,pass,":clave"));
+//            String passEncrypt = new Encrypt().encryptText(pass);
+            System.out.println("passEncrypt = "+pass);
+            parametros.add(new Parametros(CONSTANTE.CADENA,pass,"clave"));
+            Dao<Usuario> dao= new Dao<Usuario>(Usuario.class);
+            usuarios=dao.consultaQueryByParametros("Usuario.findByClaveAndUser", parametros);
+            System.out.println("Total : "+usuarios.size());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "Total : "+usuarios.size();
+    }
+}
---
+++ SGCVServicios/src/main/java/com/sgcv/dao/Persona.java
@@ -0,0 +1,238 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sgcv.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Ernesto
+ */
+@Entity
+@Table(name = "persona")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Persona.findAll", query = "SELECT p FROM Persona p")
+    , @NamedQuery(name = "Persona.findByIdPersona", query = "SELECT p FROM Persona p WHERE p.idPersona = :idPersona")
+    , @NamedQuery(name = "Persona.findByTipoPersona", query = "SELECT p FROM Persona p WHERE p.tipoPersona = :tipoPersona")
+    , @NamedQuery(name = "Persona.findByAPaterno", query = "SELECT p FROM Persona p WHERE p.aPaterno = :aPaterno")
+    , @NamedQuery(name = "Persona.findByAMaterno", query = "SELECT p FROM Persona p WHERE p.aMaterno = :aMaterno")
+    , @NamedQuery(name = "Persona.findByNombre", query = "SELECT p FROM Persona p WHERE p.nombre = :nombre")
+    , @NamedQuery(name = "Persona.findByFechaNacimiento", query = "SELECT p FROM Persona p WHERE p.fechaNacimiento = :fechaNacimiento")
+    , @NamedQuery(name = "Persona.findBySexo", query = "SELECT p FROM Persona p WHERE p.sexo = :sexo")
+    , @NamedQuery(name = "Persona.findByCurp", query = "SELECT p FROM Persona p WHERE p.curp = :curp")
+    , @NamedQuery(name = "Persona.findByRfc", query = "SELECT p FROM Persona p WHERE p.rfc = :rfc")
+    , @NamedQuery(name = "Persona.findByCorreo", query = "SELECT p FROM Persona p WHERE p.correo = :correo")
+    , @NamedQuery(name = "Persona.findByTelefono1", query = "SELECT p FROM Persona p WHERE p.telefono1 = :telefono1")
+    , @NamedQuery(name = "Persona.findByTelefono2", query = "SELECT p FROM Persona p WHERE p.telefono2 = :telefono2")})
+public class Persona implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_persona")
+    private Integer idPersona;
+    @Size(max = 1)
+    @Column(name = "tipo_persona")
+    private String tipoPersona;
+    @Size(max = 1)
+    @Column(name = "a_paterno")
+    private String aPaterno;
+    @Size(max = 1)
+    @Column(name = "a_materno")
+    private String aMaterno;
+    @Size(max = 1)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 1)
+    @Column(name = "fecha_nacimiento")
+    private String fechaNacimiento;
+    @Size(max = 1)
+    @Column(name = "sexo")
+    private String sexo;
+    @Size(max = 1)
+    @Column(name = "curp")
+    private String curp;
+    @Size(max = 1)
+    @Column(name = "rfc")
+    private String rfc;
+    @Size(max = 1)
+    @Column(name = "correo")
+    private String correo;
+    @Size(max = 1)
+    @Column(name = "telefono1")
+    private String telefono1;
+    @Size(max = 1)
+    @Column(name = "telefono2")
+    private String telefono2;
+    @OneToMany(mappedBy = "idPersona", fetch = FetchType.LAZY)
+    private List<Trabajador> trabajadorList;
+    @OneToMany(mappedBy = "idPersona", fetch = FetchType.LAZY)
+    private List<Cliente> clienteList;
+
+    public Persona() {
+    }
+
+    public Persona(Integer idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public Integer getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Integer idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getTipoPersona() {
+        return tipoPersona;
+    }
+
+    public void setTipoPersona(String tipoPersona) {
+        this.tipoPersona = tipoPersona;
+    }
+
+    public String getAPaterno() {
+        return aPaterno;
+    }
+
+    public void setAPaterno(String aPaterno) {
+        this.aPaterno = aPaterno;
+    }
+
+    public String getAMaterno() {
+        return aMaterno;
+    }
+
+    public void setAMaterno(String aMaterno) {
+        this.aMaterno = aMaterno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono1() {
+        return telefono1;
+    }
+
+    public void setTelefono1(String telefono1) {
+        this.telefono1 = telefono1;
+    }
+
+    public String getTelefono2() {
+        return telefono2;
+    }
+
+    public void setTelefono2(String telefono2) {
+        this.telefono2 = telefono2;
+    }
+
+    @XmlTransient
+    public List<Trabajador> getTrabajadorList() {
+        return trabajadorList;
+    }
+
+    public void setTrabajadorList(List<Trabajador> trabajadorList) {
+        this.trabajadorList = trabajadorList;
+    }
+
+    @XmlTransient
+    public List<Cliente> getClienteList() {
+        return clienteList;
+    }
+
+    public void setClienteList(List<Cliente> clienteList) {
+        this.clienteList = clienteList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPersona != null ? idPersona.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Persona)) {
+            return false;
+        }
+        Persona other = (Persona) object;
+        if ((this.idPersona == null && other.idPersona != null) || (this.idPersona != null && !this.idPersona.equals(other.idPersona))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.sgcv.dao.Persona[ idPersona=" + idPersona + " ]";
+    }
+    
+}
